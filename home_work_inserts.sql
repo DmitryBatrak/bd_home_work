e50b32f35a18fb26068036a1cf88a93561bd6630
Dmitry Batrak <dimanyand@gmail.com>
2024-11-21 21:16:02 +0300

    Second commit

---
+++ home_work_inserts.sql
@@ -0,0 +1,53 @@
+INSERT INTO generes_of_music 
+VALUES (1, 'Rap'),
+       (2, 'Rock'),
+       (3, 'Pop');
+           
+INSERT INTO music_performers 
+VALUES (1, 'Mac Miller'),
+       (2, 'SchoolboyQ'),
+       (3, 'Ozzy Osbourne'),
+       (4, 'Britney Spears');
+     	
+INSERT INTO list_of_albums 
+VALUES (1, 2013, 'Watching Movies with the Sound Off'),
+       (2, 2020, 'Oxymoron'),
+       (3, 1991, 'No More Tears'),
+       (4, 2003, 'In the Zone');
+      
+INSERT INTO music_tracks 
+VALUES (1, 'Watching Movies with the Sound Off', 302, 'I Am Who Am'),
+       (2, 'Watching Movies with the Sound Off', 360, 'Red Dot Music'),
+       (3, 'Oxymoron', 267, 'What They Want'),
+       (4, 'Oxymoron', 163, 'Man of the Year'),
+       (5, 'No More Tears', 260, 'Time After Time'),
+       (6, 'No More Tears', 253, 'A.V.H.'),
+       (7, 'In the Zone', 164, 'Showdown my'),
+       (8, 'In the Zone', 140, 'Toxic');
+	  
+INSERT INTO collections 
+VALUES (1, 2000, 'Top of 90-th'),
+       (2, 2010, 'Top of 00-th'),
+       (3, 2020, 'Top of 10-th');
+	  
+INSERT INTO genre_and_performens 
+VALUES (1, 1),
+       (1, 2),
+       (2, 3),
+       (3, 4);
+	  
+INSERT INTO albums_and_performers 
+VALUES (1, 1),
+       (2, 2),
+       (3, 3),
+       (4, 4);
+      
+INSERT INTO collections_and_tracks 
+VALUES (1, 3),
+       (2, 3),
+       (3, 3),
+       (4, 3),
+       (5, 1),
+       (6, 1),
+       (7, 2),
+       (8, 2);
---
+++ home_work_selects.sql
@@ -0,0 +1,52 @@
+-- Задание 2
+SELECT name_of_track AS "Назвние трека", MAX(duration) AS "Продолжительность"
+  FROM music_tracks
+ GROUP BY  name_of_track, duration
+ LIMIT 1;
+ 
+SELECT name_of_track AS "Назвние трека"
+  FROM music_tracks
+ WHERE duration > 150;
+ 
+SELECT collection_name AS "Название сборника"
+  FROM collections
+ WHERE year_of_issue BETWEEN 2018 AND 2020;
+ 
+SELECT nickname AS "Исполнитель"
+  FROM music_performers
+ WHERE nickname NOT LIKE '% %';
+ 
+SELECT name_of_track AS "Назвние трека"
+  FROM music_tracks
+ WHERE name_of_track LIKE '%my%'
+ 
+ -- Задание3
+ SELECT genre_name AS "Жанр", COUNT(*) AS "Количество"
+   FROM generes_of_music
+   JOIN genre_and_performens ON generes_of_music.id = genre_and_performens.genre_names
+  GROUP BY genre_name;
+  
+ SELECT album_name AS "Альбом", COUNT(*) AS "Количество", year_of_issue AS "Год"
+   FROM list_of_albums
+   JOIN music_tracks ON list_of_albums.album_name = music_tracks.albums_names
+  WHERE year_of_issue BETWEEN 2019 AND 2020
+  GROUP BY album_name, year_of_issue;
+ 
+SELECT albums_names AS "Название альбома", AVG(duration) AS "Средняя продолжительность"
+  FROM music_tracks
+ GROUP BY albums_names;
+
+SELECT nickname AS "Исполнитель"
+  FROM music_performers
+  JOIN albums_and_performers ON music_performers.id = albums_and_performers.nick_name 
+  JOIN list_of_albums ON albums_and_performers.albums_name = list_of_albums.id
+ WHERE year_of_issue != 2020;
+
+SELECT collection_name AS "Сборник"
+  FROM collections
+  JOIN collections_and_tracks ON collections.id = collections_and_tracks.collection_names 
+  JOIN music_tracks ON collections_and_tracks.name_of_tracks = music_tracks.id 
+  JOIN list_of_albums ON music_tracks.albums_names = list_of_albums.album_name 
+  JOIN albums_and_performers ON list_of_albums.id = albums_and_performers.albums_name
+  JOIN music_performers ON albums_and_performers.nick_name = music_performers.id 
+ WHERE nickname = 'Mac Miller';
